ca6a6ea79b5b07a8a43887d3c0dcb74fa06601e2
AtlantV <vadym.bp@gmail.com>
2026-02-07 16:44:29 +0200

    Add first code

---
+++ Task_1_get_days_from_today.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+def get_days_from_today(d):
+    input_date = datetime.strptime(d, "%Y-%m-%d").date()
+    today = datetime.today().date()
+    delta = today - input_date
+    return delta.days
+
+d = input("Please insert date (yyyy-mm-dd): ")
+result = get_days_from_today(d)
+
+print(f"Difference in days: {result}")
+
+
+
