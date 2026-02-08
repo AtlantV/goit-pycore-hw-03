ebaab36f8f58fedad738afa4e3d74ac453d4b314
AtlantV <vadym.bp@gmail.com>
2026-02-08 23:28:50 +0200

    After check review

--- main/Task_2_get_numbers_ticket.py
+++ main/Task_2_get_numbers_ticket.py
@@ -2,12 +2,12 @@ import random
 
 def get_numbers_ticket(min_value, max_value, quantity): #min/max -> min/max_value змінив умови завдання (так ніби правильніше аби не плутати з базовими функціями)
     numbers_set = set() 
-    if min >= 1 and max <= 1000: #перевірка на відповідність умовам
+    if min_value >= 1 and max_value <= 1000: #перевірка на відповідність умовам
         while len(numbers_set) < quantity: #виконаня за заданою кількістю цифр
             numbers_set.add(random.randint(min_value, max_value))
-            return sorted(numbers_set) #виконання умови відсортованого списку
+        return sorted(numbers_set) #виконання умови відсортованого списку
     else:
         return list(numbers_set) #можна було повернути мабуть просто новий пустий список, як варіант  
     
-a = get_numbers_ticket (2, 100, 15)
+a = get_numbers_ticket (2, 50, 15)
 print(a)
