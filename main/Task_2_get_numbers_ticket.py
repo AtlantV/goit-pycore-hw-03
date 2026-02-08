7a8215b61a96e12b82c55304b11fddbf79a2fea3
AtlantV <vadym.bp@gmail.com>
2026-02-08 23:50:38 +0200

    Add chek for quantity error

--- main/Task_2_get_numbers_ticket.py
+++ main/Task_2_get_numbers_ticket.py
@@ -3,11 +3,14 @@ import random
 def get_numbers_ticket(min_value, max_value, quantity): #min/max -> min/max_value змінив умови завдання (так ніби правильніше аби не плутати з базовими функціями)
     numbers_set = set() 
     if min_value >= 1 and max_value <= 1000: #перевірка на відповідність умовам
+        if quantity > (max_value - min_value + 1): #перевірка входження бажаної кількості чисел в діапазон
+            raise ValueError("Задана кількість чисел більша за визначений вами діапазон")
+        
         while len(numbers_set) < quantity: #виконаня за заданою кількістю цифр
             numbers_set.add(random.randint(min_value, max_value))
         return sorted(numbers_set) #виконання умови відсортованого списку
     else:
         return list(numbers_set) #можна було повернути мабуть просто новий пустий список, як варіант  
     
-a = get_numbers_ticket (2, 50, 15)
+a = get_numbers_ticket (1, 100, 15)
 print(a)
